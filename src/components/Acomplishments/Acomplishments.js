ca0f0629483c2efbee62b4e560d0d89dcd840fa3
hemalawati <hemalawati@Hemas-MacBook-Pro.local>
2023-07-24 17:39:23 -0400

    formatted accomplishments

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
-
-const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
-];
+import { Section, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxText } from './AcomplishmentsStyles';
+import { accomplishments } from '../../constants/constants';
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+	<Section>
+		<SectionTitle>Personal Acomplishments</SectionTitle>
+		<Boxes>
+			{accomplishments.map((card, index) => (
+				<Box key={index}>
+					<BoxText>{card.text}</BoxText>
+				</Box>
+			))}
+		</Boxes>
+	</Section>
 );
 
 export default Acomplishments;
